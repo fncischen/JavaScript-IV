ccf626fbfe506c15d7ce29481c9e1ea13daf26e8
Francis Chen <fncis.chen@gmail.com>
2019-01-24 14:35:59 -0800

    added all relevant classes on lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,7 +9,7 @@ class Person {
     }
 
     speak() {
-        return "Hello, my name is $(this.name) and I am from $(this.location)";
+        return `Hello, my name is ${this.name} and I am from ${this.location}.`;
     }
 }
 
@@ -22,11 +22,11 @@ class Instructor extends Person {
     }
 
     demo(subject) {
-        return "Today we are learning about $(subject)";
+        return `Today we are learning about ${subject}`;
     };
 
     grade(student, subject) {
-        return "$(student.name) received a perfect score on $(subject)."
+        return `${student.name} received a perfect score on ${subject}.`;
     };
 
 }
@@ -40,10 +40,82 @@ class Student extends Person {
     }
 
     listSubjects() {
-        this.favSubjects.foreach(function(element) {
+        favSubjects.foreach(function(element) {
            return element; 
-        }
+        });
+    };
+
+    PRAssignment(subject) {
+        return `${this.name} submitted a PR for ${subject}.` ;
+    };
+
+    sprintChallenge(subject) {
+        return `${this.name} has begune sprint challenge on ${subject}.`;
+    };
+}
+
+class ProjectManager extends Instructor {
+    constructor(PMStats) {
+        super(PMStats);
+        this.gradClassName = PMStats.gradClassName;
+        this.favInstructor = PMStats.favInstructor;
     }
 
-    
+    standUp(channel) {
+        return `${this.name} announces to ${channel}: ${channel} study time!`;
+    }
+
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}.`;
+    }
 }
+
+// Example Classes:
+
+const Jan = new Instructor({
+    name: "Jan",
+    age: 35,
+    location: "Denver",
+    gender: "Male",
+
+    specialty: "Python",
+    favLangauge: "Javascript",
+    catchPhrase: "Life's a stage."
+})
+
+const Suzy = new Student({
+    name: "Suzy",
+    age: 30,
+    location: "Salt Lake City",
+    gender: "Female",
+
+    previousBackground: "Chemical Engineer",
+    className: "WEB17",
+    favSubjects: ["Chemistry", "Math"]
+})
+
+const Joe = new ProjectManager({
+    name: "Joe",
+    age: 37,
+    location: "Portland",
+    gender: "male",
+
+    gradClassName: "WEB15",
+    favInstructor: "Jan"
+})
+
+// 
+
+console.log(Joe.age); //? 
+console.log(Suzy.previousBackground); //?
+console.log(Jan.catchPhrase); //?
+
+console.log(Jan.demo("DS10")); //?
+console.log(Jan.grade(Suzy,"DS10")); //?
+
+console.log(Joe.standUp("WEB17Channel")) //?
+console.log(Joe.debugsCode(Suzy, "DS10")) //?
+
+console.log(Suzy.speak()); //?
+console.log(Suzy.PRAssignment("DS10")); //?
+console.log(Suzy.sprintChallenge("DS10")); //?
