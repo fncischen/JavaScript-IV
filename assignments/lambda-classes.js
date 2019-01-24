f1862c9f6fc80c0d2ec11a2cadc2bdbfa939b79a
Francis Chen <fncis.chen@gmail.com>
2019-01-24 13:47:15 -0800

    updated lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,49 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(personStats){
+        this.name = personStats.name;
+        this.age = personStats.age;
+        this.location = personStats.location;
+        this.gender = personStats.gender;
+    }
+
+    speak() {
+        return "Hello, my name is $(this.name) and I am from $(this.location)";
+    }
+}
+
+class Instructor extends Person {
+    constructor(InstructorStats){
+        super(InstructorStats);
+        this.specialty = InstructorStats.specialty;
+        this.favLangauge = InstructorStats.favLangauge;
+        this.catchPhrase = InstructorStats.catchPhrase;
+    }
+
+    demo(subject) {
+        return "Today we are learning about $(subject)";
+    };
+
+    grade(student, subject) {
+        return "$(student.name) received a perfect score on $(subject)."
+    };
+
+}
+
+class Student extends Person {
+    constructor(StudentStats) {
+        super(StudentStats);
+        this.previousBackground = StudentStats.previousBackground;
+        this.className = StudentStats.className;
+        this.favSubjects = StudentStats.favSubjects;
+    }
+
+    listSubjects() {
+        this.favSubjects.foreach(function(element) {
+           return element; 
+        }
+    }
+
+    
+}
